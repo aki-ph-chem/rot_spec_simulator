a71953491cc95f0569c03fd4451bb4e792bfff5b
aki <aki@mymail.com>
2024-03-11 23:57:22 +0900

    implemented anothor version of Boltzman factor

--- diatomic_simulator.py
+++ diatomic_simulator.py
@@ -19,7 +19,33 @@ class Population:
         energy_vec = np.vectorize(self.molecule.energy)
 
         wn = 2*self.molecule.rot_const *(x + 1)
+        '''
+        Z: partition function
+        boltzaman factor: exp(-E_{upper}/k_BT) / Z 
+        '''
         boltzman_factor = np.exp(-energy_vec(x) /self.temp)
+        population_array = (x + 1) * boltzman_factor 
+        intensity = population_array / ((2* x + 1) * boltzman_factor).sum() 
+
+        return (wn, intensity)
+
+class Population2:
+    def __init__(self, temp: float, j_max: int, rot_const: float) -> None:
+        self.temp = temp
+        self.j_max = j_max
+        self.molecule = DiatomicMolecule(rot_const)
+
+    def spectrum(self) -> tuple[np.ndarray, np.ndarray]:
+        x = np.arange(0, self.j_max, 1)
+        energy_vec = np.vectorize(self.molecule.energy)
+
+        wn = 2*self.molecule.rot_const *(x + 1)
+        '''
+        Z: partition function
+        boltzaman factor: exp(-E_{upper}/k_BT) /Z - exp(-E_{lower}/k_BT) /Z
+        '''
+        boltzman_factor = np.exp(-energy_vec(x) /self.temp) - np.exp(-energy_vec(x + 1) /self.temp)
+
         population_array = (x + 1) * boltzman_factor 
         intensity = population_array / ((2* x + 1) * boltzman_factor).sum() 
 
@@ -27,10 +53,15 @@ class Population:
 
 def main() -> None:
     pop_1 = Population(300, 30, 2.0)
-
     spectrum_1 = pop_1.spectrum()
+
+    pop_2 = Population2(300, 30, 2.0)
+    spectrum_2 = pop_2.spectrum()
+
     plt.scatter(spectrum_1[0], spectrum_1[1], color = 'red')
+    plt.scatter(spectrum_2[0], spectrum_2[1], color = 'blue')
     plt.show()
+
 
 if __name__ == "__main__":
     main()
